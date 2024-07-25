471a382f2a06b44ffab136fbc09a2b9f184cd368
Ota-Prokopec <otaprokopec@gmail.com>
2024-07-25 10:50:44 +0200

    blur effect in iframe

--- apps/personal-otaprokopec-web/src/components/common/OtaProkopecIframe.tsx
+++ apps/personal-otaprokopec-web/src/components/common/OtaProkopecIframe.tsx
@@ -8,17 +8,18 @@ export type OtaProkopecIframeProps = {}
 
 export const OtaProkopecIframe = ({}: OtaProkopecIframeProps) => {
 	return (
-		<Column className="w-full h-auto gap-4">
+		<Column className="w-full h-auto gap-4 relative">
 			<Iframe
 				scrolling="no"
-				className="w-full overflow-hidden h-[800px]"
+				className="w-full overflow-hidden h-[800px] z-10"
 				src="https://www.otaprokopec.cz"
 			></Iframe>
-			<Center className="mt-[-50px]">
+			<Center className="mt-[-50px] z-30 top-[-10px] relative">
 				<ButtonLink href="https://www.otaprokopec.cz" className="mobile:w-full w-max ">
 					Load more ...
 				</ButtonLink>
 			</Center>
+			<div className=" w-full h-[200px]  absolute bottom-0 left-0 z-20  overflow-visible bg-[linear-gradient(to_top,grey,transparent)] rounded-b-3xl"></div>
 		</Column>
 	)
 }
